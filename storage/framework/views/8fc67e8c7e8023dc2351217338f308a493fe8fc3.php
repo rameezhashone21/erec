23196fcf41922f55e6cbacb7576ed5cf4f8a9346
rameezhashone21 <rameez.hashone@gmail.com>
2024-09-09 17:50:28 +0500

    Stop tracking storage/framework

--- storage/framework/views/8fc67e8c7e8023dc2351217338f308a493fe8fc3.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>
-
-<?php /**PATH /home/backendhostingla/public_html/webapp/erec/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/subcopy.blade.php ENDPATH**/ ?>
--- storage/framework/views/c322a73bf502379c6f846eeda15eefb51696b6dc.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH /home/backendhostingla/public_html/webapp/erec/vendor/laravel/framework/src/Illuminate/Mail/resources/views/text/header.blade.php ENDPATH**/ ?>
