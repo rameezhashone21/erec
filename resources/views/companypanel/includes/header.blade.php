e1414c6f7a2cfa25a4cfb21812bfa9ddeb758641
rameezhashone21 <rameez.hashone@gmail.com>
2024-09-11 17:22:07 +0500

    updated

--- resources/views/companypanel/includes/header.blade.php
+++ resources/views/companypanel/includes/header.blade.php
@@ -393,12 +393,12 @@
                                 <a href=""
                                     class="d-flex fs-12 btn-subcription {{ auth()->user()->package->class }}"
                                     style="font-size: 10px !important">
-                                    {{ auth()->user()->posts_count }}/
+                                    {{ auth()->user()->all_posts_count }}/
                                     {{-- @if (auth()->user()->posts_count > auth()->user()->package->no_of_posts)
                                         {{ (auth()->user()->package->no_of_posts + auth()->user()->posts_count) - auth()->user()->package->no_of_posts }}
                                         jobs Posts left
                                     @else --}}
-                                    {{ auth()->user()->all_posts_count }} Jobs posts left
+                                    {{ auth()->user()->posts_count }} Jobs posts left
                                     {{-- @endif --}}
                                 </a>
                                 {{-- @if (auth()->user()->package->name == 'Standard')
